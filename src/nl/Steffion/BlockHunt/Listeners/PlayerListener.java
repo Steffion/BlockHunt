48d6a61e06e585a46f49669e9f76350e2e1b62c7
Steffion <stefdegoey@gmail.com>
2013-07-30 21:08:50 +0200

    Removed old code.

--- src/nl/Steffion/BlockHunt/Listeners/PlayerListener.java
+++ src/nl/Steffion/BlockHunt/Listeners/PlayerListener.java
@@ -81,29 +81,4 @@ public class PlayerListener implements Listener {
 			}
 		}
 	}
-	// @EventHandler(priority = EventPriority.MONITOR)
-	// public void onPlayerTeleportEvent(final PlayerTeleportEvent event) {
-	// final Player player = event.getPlayer();
-	// if (W.previewWorlds.contains(event.getFrom().getWorld().getName())) {
-	// if (!event.getFrom().getWorld().getName()
-	// .equals(event.getTo().getWorld().getName())) {
-	// final String worldLoc = event.getFrom().getWorld().getName();
-	// boolean unload = true;
-	//
-	// for (Player pl : Bukkit.getOnlinePlayers()) {
-	// if (pl.getLocation().getWorld().getName().equals(worldLoc)) {
-	// if (!pl.equals(player)) {
-	// unload = false;
-	// }
-	// }
-	// }
-	//
-	// if (unload) {
-	// ArenaHandler ah = new ArenaHandler(this.plugin);
-	// ah.unLoadArena(worldLoc, player);
-	// W.previewWorlds.remove(worldLoc);
-	// }
-	// }
-	// }
-	// }
 }
