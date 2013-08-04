49efe457ec132e64edd3f2a5360660bf56d3f818
Steffion <stefdegoey@gmail.com>
2013-08-05 00:02:22 +0200

    Won't lose food levels while ingame.

---
+++ src/nl/Steffion/BlockHunt/Listeners/OnFoodLevelChangeEvent.java
@@ -0,0 +1,24 @@
+package nl.Steffion.BlockHunt.Listeners;
+
+import nl.Steffion.BlockHunt.Arena;
+import nl.Steffion.BlockHunt.W;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.FoodLevelChangeEvent;
+
+public class OnFoodLevelChangeEvent implements Listener {
+
+	@EventHandler(priority = EventPriority.NORMAL)
+	public void onFoodLevelChangeEvent(FoodLevelChangeEvent event) {
+		Player player = (Player) event.getEntity();
+
+		for (Arena arena : W.arenaList) {
+			if (arena.playersInArena.contains(player)) {
+				event.setCancelled(true);
+			}
+		}
+	}
+}
