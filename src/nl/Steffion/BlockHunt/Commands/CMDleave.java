c83f18da9fbd975db9aebe8564b802704d5f9c3a
Steffion <stefdegoey@gmail.com>
2013-08-05 00:01:26 +0200

    Changed way to join and leave. Also created the leave command.

--- src/nl/Steffion/BlockHunt/Commands/CMDleave.java
+++ src/nl/Steffion/BlockHunt/Commands/CMDleave.java
@@ -1,7 +1,6 @@
 package nl.Steffion.BlockHunt.Commands;
 
 import nl.Steffion.BlockHunt.ArenaHandler;
-import nl.Steffion.BlockHunt.Managers.CommandC;
 import nl.Steffion.BlockHunt.Managers.ConfigC;
 import nl.Steffion.BlockHunt.Managers.MessageM;
 import nl.Steffion.BlockHunt.Managers.PlayerM;
@@ -9,20 +8,14 @@ import nl.Steffion.BlockHunt.Managers.PlayerM.PermsC;
 import org.bukkit.command.Command;
 import org.bukkit.entity.Player;
 
-public class CMDjoin extends DefaultCMD {
+public class CMDleave extends DefaultCMD {
 
 	@Override
 	public boolean exectue(Player player, Command cmd, String label,
 			String[] args) {
-		if (PlayerM.hasPerm(player, PermsC.join, true)) {
+		if (PlayerM.hasPerm(player, PermsC.leave, true)) {
 			if (player != null) {
-				if (args.length <= 1) {
-					MessageM.sendFMessage(player,
-							ConfigC.error_notEnoughArguments, true, "syntax-"
-									+ CommandC.JOIN.usage);
-				} else {
-					ArenaHandler.playerJoinArena(player, args[1]);
-				}
+				ArenaHandler.playerLeaveArena(player);
 			} else {
 				MessageM.sendFMessage(player, ConfigC.error_onlyIngame, true);
 			}
