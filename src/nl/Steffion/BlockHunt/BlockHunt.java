7380f885a30432d6de2aef26f3b95152973ce7fa
addstar <addstar@addstar.com.au>
2015-01-25 00:40:00 +1100

    Remove accidental unnecessary import

--- src/nl/Steffion/BlockHunt/BlockHunt.java
+++ src/nl/Steffion/BlockHunt/BlockHunt.java
@@ -58,7 +58,6 @@ import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.util.StringUtil;
 
 public class BlockHunt extends JavaPlugin implements Listener {
 	/**
