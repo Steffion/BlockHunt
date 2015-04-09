fff822c93392af29cb66c402beb06f0650082a1f
Steffion <steffion@icloud.com>
2015-04-09 20:00:23 +0200

    Removed old code

--- src/nl/Steffion/BlockHunt/Serializables/M.java
+++
@@ -1,9 +0,0 @@
-package nl.Steffion.BlockHunt.Serializables;
-
-import java.util.Map;
-
-public class M {
-	public static Object g(Map<String, Object> map, String key, Object def) {
-		return (map.containsKey(key) ? map.get(key) : def);
-	}
-}
