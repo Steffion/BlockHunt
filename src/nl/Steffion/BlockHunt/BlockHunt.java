f3c097f9245d5b252eb3f24f798bead3becd254d
Steffion <steffion@icloud.com>
2016-01-17 20:48:46 +0100

    Created default project files

---
+++ src/nl/Steffion/BlockHunt/BlockHunt.java
@@ -0,0 +1,17 @@
+package nl.Steffion.BlockHunt;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BlockHunt extends JavaPlugin {
+
+	@Override
+	public void onDisable() {
+	
+	}
+
+	@Override
+	public void onEnable() {
+	
+	}
+	
+}
