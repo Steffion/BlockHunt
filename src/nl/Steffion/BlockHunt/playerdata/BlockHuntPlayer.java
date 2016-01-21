cad91d9b2052513b57bd0a563602cbdde65a6061
Steffion <steffion@icloud.com>
2016-01-21 17:13:09 +0100

    Added health and food level to the backup player

--- src/nl/Steffion/BlockHunt/playerdata/BlockHuntPlayer.java
+++ src/nl/Steffion/BlockHunt/playerdata/BlockHuntPlayer.java
@@ -10,6 +10,8 @@ public class BlockHuntPlayer {
 	private boolean		allowFlight;
 	private ItemStack[]	armorContents;
 	private float		exp;
+	private int			foodLevel;
+	private double		health;
 	private ItemStack[]	inventoryContents;
 	private int			level;
 	private UUID		uuid;
@@ -18,13 +20,13 @@ public class BlockHuntPlayer {
 		Player player = Bukkit.getPlayer(uuid);
 
 		player.setAllowFlight(false);
-
 		player.getInventory().setHelmet(null);
 		player.getInventory().setChestplate(null);
 		player.getInventory().setLeggings(null);
 		player.getInventory().setBoots(null);
-
 		player.setExp(0);
+		player.setFoodLevel(20);
+		player.setHealth(20);
 
 		for (int i = 0; i < inventoryContents.length; i++) {
 			player.getInventory().setItem(i, null);
@@ -37,13 +39,13 @@ public class BlockHuntPlayer {
 		Player player = Bukkit.getPlayer(uuid);
 		
 		player.setAllowFlight(allowFlight);
-		
 		player.getInventory().setHelmet(armorContents[3]);
 		player.getInventory().setChestplate(armorContents[2]);
 		player.getInventory().setLeggings(armorContents[1]);
 		player.getInventory().setBoots(armorContents[0]);
-		
 		player.setExp(exp);
+		player.setFoodLevel(foodLevel);
+		player.setHealth(health);
 		
 		for (int i = 0; i < inventoryContents.length; i++) {
 			player.getInventory().setItem(i, inventoryContents[i]);
@@ -56,6 +58,8 @@ public class BlockHuntPlayer {
 		allowFlight = player.getAllowFlight();
 		armorContents = player.getInventory().getArmorContents();
 		exp = player.getExp();
+		foodLevel = player.getFoodLevel();
+		health = player.getHealth();
 		inventoryContents = player.getInventory().getContents();
 		level = player.getLevel();
 		uuid = player.getUniqueId();
